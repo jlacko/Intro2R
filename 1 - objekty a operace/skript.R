4aed49d43ae2db5d4f512896a99ed37c0ef683c9
Jindra Lacko <jindra.lacko@gmail.com>
2019-09-10 14:10:04 +0200

    extras renamed, markdown + script created

---
+++ 1 - objekty a operace/skript.R
@@ -0,0 +1,13 @@
+# příklad erkového skriptu - obsahuje pouze kód (a kometáře k němu)
+
+# do proměnné "text" uložit textovou konstantu
+text <- "Karel"
+
+# do proměnné "cislo" uložit číselnou konstantu
+cislo <- c(1, 2, 3)
+
+# na konzoli vypsat "Máma má mísu"
+print("Máma má mísu")
+
+# funkce: na konzoli vypsat obsah textu
+print(text)

129ef4066525767c6c8d3d6a2a574879939e90ae
Jindra Lacko <jindra@jla-data.net>
2019-10-04 22:22:41 +0000

    stěrače stírají, konektor konektuje

--- extras/read-db.R
+++ extras/read-db.R
@@ -1,11 +1,23 @@
 library(tidyverse)
+library(dbplyr)
 library(odbc)
 library(DBI)
 
-con <- dbConnect(odbc::odbc(), .connection_string = "Driver={PostgreSQL Unicode};Server=db.jla-data.net;Port=5432;Database=R4SU;Uid=R4SU;Pwd=R4SU;", timeout = 10)
+con <- dbConnect(odbc::odbc(), driver = "PostgreSQL Unicode",
+                 server = "db.jla-data.net", port = 5432, uid = "R4SU",
+                 password = "R4SU", database = "R4SU")
 
-sql_code <- readLines("./extras/potraviny.sql")
+# vytvořím query o kapříkovi na Vánoce
+kaprik <- tbl(con, "potraviny") %>% 
+   filter(reprcen_kod == "2000001"
+          & uzemi_kod != 19
+          & obdobiod >= "2017-12-01" 
+          & obdobido <= "2018-01-01") 
 
-asdf <- dbGetQuery(con, sql_code)
+# prohlédnu si sql kód odpovídající kapříkovi (zatím žádná exekuce!)
+sql_render(kaprik)
+
+# exekuce kódu - teprve teď se pustí kód proti databázi
+realny_kaprik <- collect(kaprik)
 
 dbDisconnect(con) # uklidit po sobě je slušnost...
